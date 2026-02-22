1f927d1434eab377414fc01dd0a1eecf3123c29e
ShreyaChoudhury5656 <shreyachoudhuryofficial@gmail.com>
2026-02-23 01:38:11 +0530

    UC4 completed

---
+++ OOPSBannerApp4.java
@@ -0,0 +1,49 @@
+public class OOPSBannerApp4 {
+
+    public static void main(String[] args) {
+
+        String[] banner = {
+      String.join("\n",
+                " *** ",
+                "**  **",
+                "**  **",
+                "**  **",
+                "**  **",
+                " *** "
+        ),
+
+      String.join("\n",
+                " *** ",
+                "**  **",
+                "**  **",
+                "**  **",
+                "**  **",
+                " *** "
+        ),
+
+        String.join("\n",
+                "***** ",
+                "**  **",
+                "***** ",
+                "**     ",
+                "**     ",
+                "**     "
+        ),
+
+        String.join("\n",
+                " *****",
+                "**     ",
+                " **** ",
+                "     **",
+                "     **",
+                "***** "
+        )
+};
+
+    for (int i = 0; i < banner.length; i++) {
+      System.out.println(banner[i]);
+}
+   }}
+
+
+             
